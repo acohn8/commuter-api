c0a7facc2f695663e1e7b870ddd760361b45e67d
Adam Cohn <adamcohn88@gmail.com>
2019-01-04 17:06:33 -0500

    better gql implementation

--- app/graphql/types/station_type.rb
+++ app/graphql/types/station_type.rb
@@ -1,7 +1,6 @@
 class Types::StationType < Types::BaseObject
-  description "A station"
+  description 'A station'
 
-  # it has the following fields
   field :id, ID, null: false
   field :name, String, null: false
   field :address, String, null: false
@@ -9,4 +8,27 @@ class Types::StationType < Types::BaseObject
   field :lng, Float, null: false
   field :lines, [Types::LineType], null: false
   field :station_codes, [Types::StationCodeType], null: false
+  field :trains, [Types::TrainType], null: false
+  field :distance, Float, null: true do
+    argument :lat, Float, required: true
+    argument :lng, Float, required: true
+  end
+
+  def trains
+    all_trains = []
+    codes = station_codes.map { |c| c.station_code }
+    codes.each do |code|
+      url = "https://dcmetrohero.com/api/v1/metrorail/stations/#{code}/trains"
+      headers = { apiKey: 'bffa0ad2-d611-4025-a289-2bb5a50ee8e7' }
+      result = HTTParty.get(url, headers: headers)
+      trains = JSON.parse(result.response.body)
+      all_trains << trains
+    end
+    all_trains.flatten
+  end
+
+  def distance(lat:, lng:)
+    station = Station.find(id)
+    station.distance_to([lat, lng])
+  end
 end
